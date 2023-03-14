421f4fa091f54df78fbedb06305fb28eaf515edb
ShalaevPavel <lohgewt123@yandex.by>
2023-03-14 21:40:40 +0300

    Revert "Revert "removed" oof"
    This reverts commit efaab722fdfb7834f09d3e793e5412df6ac9b4a4.

--- DesignPatternsColloquium/src/main/java/AbstractFactoryChoseDB/Choosing.java
+++
@@ -1,9 +0,0 @@
-package AbstractFactoryChoseDB;
-
-public class Choosing {
-    public static void main(String[] args) {
-        DB dbType = ChooseTypeOfDB.chooseDBType("Relational");
-        DistinctDB db = dbType.chooseDistinctDB("MySQL");
-        System.out.println(db.add("New note "));
-    }
-}
